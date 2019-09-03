fa92409d1c7a04dadc354c7d0e16df356fc3cf67
Avleen Dhaliwal <avleen.dhaliwal@shopify.com>
2019-09-03 18:15:57 -0400

    create and show Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,16 @@
 class ArticlesController < ApplicationController
+
   def new
   end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :text))
+    @article.save
+    redirect_to @article
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+  
 end
